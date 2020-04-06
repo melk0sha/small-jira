80d8b14fd5ae0aac71d957d0221b3360137896fd
melk0sha <din.ivanova13@gmail.com>
2020-04-06 18:02:39 +0300

    feat: add routers and its processing

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,42 @@ const usersService = require("./user.service");
 
 router.route("/").get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
   res.json(users.map(User.toResponse));
+});
+
+router.route("/:id").get(async (req, res) => {
+  const user = await usersService.getById(req.params.id);
+  res.json(User.toResponse(user));
+});
+
+router.route("/").post(async (req, res) => {
+  await usersService.create(req.body);
+  if (req.body) {
+    res.status(200).send("The user has been created");
+  } else {
+    res.status(400).send("Bad request");
+  }
+  // res.status(401)
+});
+
+router.route("/:id").put(async (req, res) => {
+  await usersService.update(req.params.id, req.body);
+  if (req.body) {
+    res.status(200).send("The user has been updated");
+  } else {
+    res.status(400).send("Bad request");
+  }
+  // res.status(401)
+});
+
+router.route("/:id").delete(async (req, res) => {
+  await usersService.deleteUser(req.params.id);
+  if (req.body) {
+    res.status(200).send("The user has been deleted");
+  } else {
+    res.status(400).send("Bad request");
+  }
+  // res.status(401)
 });
 
 module.exports = router;

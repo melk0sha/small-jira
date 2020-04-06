688397109446efc475050b2a2df254b1d2d5f702
melk0sha <din.ivanova13@gmail.com>
2020-04-06 18:01:02 +0300

    feat: add implementations of all requests

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,128 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const fs = require("fs");
+const path = require("path");
+const User = require("./user.model");
+
+const getAllUsers = async () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.join(__dirname, "../../public/users.json"),
+      "utf-8",
+      (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(data));
+        }
+      }
+    );
+  });
 };
 
-module.exports = { getAll };
+const getUserById = async id => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.join(__dirname, "../../public/users.json"),
+      "utf-8",
+      (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(data).find(user => user.id === id));
+        }
+      }
+    );
+  });
+};
+
+const createUser = async user => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.join(__dirname, "../../public/users.json"),
+      "utf-8",
+      (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const dataWithNewUser = JSON.stringify([
+            ...JSON.parse(data),
+            new User(user)
+          ]);
+
+          fs.writeFile(
+            path.join(__dirname, "../../public/users.json"),
+            dataWithNewUser,
+            () => {
+              resolve();
+            }
+          );
+        }
+      }
+    );
+  });
+};
+
+const updateUser = async (id, user) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.join(__dirname, "../../public/users.json"),
+      "utf-8",
+      (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const parsedData = JSON.parse(data);
+          const userIndex = parsedData.findIndex(
+            userData => userData.id === id
+          );
+          parsedData.splice(userIndex, 1, new User(user));
+          const updatedData = JSON.stringify(parsedData);
+
+          fs.writeFile(
+            path.join(__dirname, "../../public/users.json"),
+            updatedData,
+            () => {
+              resolve();
+            }
+          );
+        }
+      }
+    );
+  });
+};
+
+const deleteUser = async id => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.join(__dirname, "../../public/users.json"),
+      "utf-8",
+      (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const parsedData = JSON.parse(data);
+          const userIndex = parsedData.findIndex(
+            userData => userData.id === id
+          );
+          parsedData.splice(userIndex, 1);
+          const updatedData = JSON.stringify(parsedData);
+
+          fs.writeFile(
+            path.join(__dirname, "../../public/users.json"),
+            updatedData,
+            () => {
+              resolve();
+            }
+          );
+        }
+      }
+    );
+  });
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+};

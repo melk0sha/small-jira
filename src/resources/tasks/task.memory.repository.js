a7f6313fc6aebeae703d1166bee89109796b78c9
melk0sha <din.ivanova13@gmail.com>
2020-04-06 18:39:08 +0300

    feat: add template functions to work with data for tasks

---
+++ src/resources/tasks/task.memory.repository.js
@@ -0,0 +1,17 @@
+// const fs = require("fs");
+// const path = require("path");
+// const Task = require("./task.model");
+
+const getAllTasks = async () => {};
+const getTaskById = async () => {};
+const createTask = async () => {};
+const updateTask = async () => {};
+const deleteTask = async () => {};
+
+module.exports = {
+  getAllTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask
+};

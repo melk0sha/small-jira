198583592aef96eafd419fab48bdcf8cc8718a1f
melk0sha <din.ivanova13@gmail.com>
2020-04-06 18:31:20 +0300

    feat: add board with columns class

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,10 +1,27 @@
 const uuid = require("uuid");
 
 class Board {
-  constructor({ id = uuid(), title = "Board title", columns } = {}) {
+  constructor({
+    id = uuid(),
+    title = "Board title",
+    columns = [new Column()]
+  } = {}) {
     this.id = id;
     this.title = title;
-    this.columns = columns;
+    this.columns = columns.map(column => new Column(column));
+  }
+
+  addColumn(column) {
+    const [columns] = this;
+    this.columns = [...columns, new Column(column)];
+  }
+}
+
+class Column {
+  constructor({ id = uuid(), title = "Board title", order } = {}) {
+    this.id = id;
+    this.title = title;
+    this.order = order;
   }
 }
 

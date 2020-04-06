1951cbb32ec3b68783ff603b2428dcebd1481dcb
melk0sha <din.ivanova13@gmail.com>
2020-04-06 18:24:50 +0300

    feat: add board fouter and its treatment

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -1,11 +1,44 @@
 const router = require("express").Router();
-const User = require("./board.model");
-const usersService = require("./board.service");
+const boardsService = require("./board.service");
 
 router.route("/").get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  const boards = await boardsService.getAllBoards();
+  res.json(boards);
+});
+
+router.route("/:id").get(async (req, res) => {
+  const board = await boardsService.getBoardById(req.params.id);
+  res.json(board);
+});
+
+router.route("/").post(async (req, res) => {
+  await boardsService.createBoard(req.body);
+  if (req.body) {
+    res.status(200).send("The board has been created");
+  } else {
+    res.status(400).send("Bad request");
+  }
+  // res.status(401)
+});
+
+router.route("/:id").put(async (req, res) => {
+  await boardsService.updateBoard(req.params.id, req.body);
+  if (req.body) {
+    res.status(200).send("The user has been updated");
+  } else {
+    res.status(400).send("Bad request");
+  }
+  // res.status(401)
+});
+
+router.route("/:id").delete(async (req, res) => {
+  await boardsService.deleteBoard(req.params.id);
+  if (req.body) {
+    res.status(200).send("The user has been deleted");
+  } else {
+    res.status(400).send("Bad request");
+  }
+  // res.status(401)
 });
 
 module.exports = router;

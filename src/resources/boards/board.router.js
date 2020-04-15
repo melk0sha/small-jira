8bc94642a795918d3dd6fe00921d4e513bbc8b6f
melk0sha <din.ivanova13@gmail.com>
2020-04-15 15:02:59 +0300

    feat: add boards error handling

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -1,52 +1,63 @@
 const router = require("express").Router();
 const boardsService = require("./board.service");
+const RequestError = require("../../common/requestError");
 
-router.route("/").get(async (req, res) => {
-  const boards = await boardsService.getAllBoards();
-  res.json(boards);
-});
-
-router.route("/:id").get(async (req, res) => {
-  const board = await boardsService.getBoardById(req.params.id);
-  if (!req.params.id) {
-    res.status(400).send("Bad request");
-  } else if (!board) {
-    res.status(404).send("Not Found");
-  } else {
-    res.json(board);
-  }
-});
-
-router.route("/").post(async (req, res) => {
-  const board = await boardsService.createBoard(req.body);
-  if (!req.body) {
-    res.status(400).send("Bad request");
-  } else if (!board) {
-    res.status(404).send("Not Found");
-  } else {
-    res.json(board);
-  }
-});
-
-router.route("/:id").put(async (req, res) => {
-  const boards = await boardsService.updateBoard(req.params.id, req.body);
-  if (!req.params.id || !req.body) {
-    res.status(400).send("Bad request");
-  } else if (!boards) {
-    res.status(404).send("Not Found");
-  } else {
+router.route("/").get(async (req, res, next) => {
+  try {
+    const boards = await boardsService.getAllBoards();
     res.json(boards);
+    return next();
+  } catch (err) {
+    return next(err);
   }
 });
 
-router.route("/:id").delete(async (req, res) => {
-  const boards = await boardsService.deleteBoard(req.params.id);
-  if (!req.params.id) {
-    res.status(400).send("Bad request");
-  } else if (!boards) {
-    res.status(404).send("Not Found");
-  } else {
+router.route("/:id").get(async (req, res, next) => {
+  try {
+    if (!req.params.id) {
+      throw new RequestError(400, "ID not found");
+    }
+    const board = await boardsService.getBoardById(req.params.id);
+    if (!board) {
+      throw new RequestError(404, "Board not found");
+    }
+    res.json(board);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.route("/").post(async (req, res, next) => {
+  try {
+    const board = await boardsService.createBoard(req.body);
+    res.json(board);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.route("/:id").put(async (req, res, next) => {
+  try {
+    if (!req.params.id) {
+      throw new RequestError(400, "ID not found");
+    }
+    const boards = await boardsService.updateBoard(req.params.id, req.body);
     res.json(boards);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.route("/:id").delete(async (req, res, next) => {
+  try {
+    const boards = await boardsService.deleteBoard(req.params.id);
+    res.json(boards);
+    return next();
+  } catch (err) {
+    return next(err);
   }
 });
 

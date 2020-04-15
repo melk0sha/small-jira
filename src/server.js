58686e660ebbabb63de0c9902a4bfc7a2b881ea9
melk0sha <din.ivanova13@gmail.com>
2020-04-15 15:05:50 +0300

    feat: add process exception and rejection handlers

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
 const { PORT } = require("./common/config");
 const app = require("./app");
+const logger = require("./common/logger");
 
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
+
+process.on("uncaughtException", err => {
+  const exit = process.exit;
+  logger.error(`Uncaught exception: ${err.message}`);
+  exit(1);
+});
+
+process.on("unhandledRejection", reason => {
+  const exit = process.exit;
+  logger.error(`Unhandled rejection: ${reason.message}`);
+  exit(1);
+});

5ee5cc8082b2775d6e3640df4e9d2deec7ba13bd
melk0sha <din.ivanova13@gmail.com>
2020-04-15 15:03:17 +0300

    feat: add users error handling

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,53 +1,76 @@
 const router = require("express").Router();
 const User = require("./user.model");
 const usersService = require("./user.service");
+const RequestError = require("../../common/requestError");
 
-router.route("/").get(async (req, res) => {
-  const users = await usersService.getAllUsers();
-  res.json(users.map(User.toResponse));
-});
-
-router.route("/:id").get(async (req, res) => {
-  const user = await usersService.getUserById(req.params.id);
-  if (!req.params.id) {
-    res.status(400).send("Bad request");
-  } else if (!user) {
-    res.status(404).send("Not Found");
-  } else {
-    res.json(User.toResponse(user));
-  }
-});
-
-router.route("/").post(async (req, res) => {
-  const user = await usersService.createUser(req.body);
-  if (!req.body) {
-    res.status(400).send("Bad request");
-  } else if (!user) {
-    res.status(404).send("Not Found");
-  } else {
-    res.json(User.toResponse(user));
-  }
-});
-
-router.route("/:id").put(async (req, res) => {
-  const users = await usersService.updateUser(req.params.id, req.body);
-  if (!req.body) {
-    res.status(400).send("Bad request");
-  } else if (!users) {
-    res.status(404).send("Not Found");
-  } else {
+router.route("/").get(async (req, res, next) => {
+  try {
+    const users = await usersService.getAllUsers();
     res.json(users.map(User.toResponse));
+    return next();
+  } catch (err) {
+    return next(err);
   }
 });
 
-router.route("/:id").delete(async (req, res) => {
-  const users = await usersService.deleteUser(req.params.id);
-  if (!req.params.id) {
-    res.status(400).send("Bad request");
-  } else if (!users) {
-    res.status(404).send("Not Found");
-  } else {
+router.route("/:id").get(async (req, res, next) => {
+  try {
+    if (!req.params.id) {
+      throw new RequestError(400, "ID not found");
+    }
+
+    const user = await usersService.getUserById(req.params.id);
+    if (!user) {
+      throw new RequestError(404, "User not found");
+    }
+
+    res.json(User.toResponse(user));
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.route("/").post(async (req, res, next) => {
+  try {
+    const user = await usersService.createUser(req.body);
+
+    res.json(User.toResponse(user));
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.route("/:id").put(async (req, res, next) => {
+  try {
+    if (!req.params.id) {
+      throw new RequestError(400, "ID not found");
+    }
+
+    const users = await usersService.updateUser(req.params.id, req.body);
     res.json(users.map(User.toResponse));
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.route("/:id").delete(async (req, res, next) => {
+  try {
+    if (!req.params.id) {
+      throw new RequestError(400, "ID not found");
+    }
+
+    const users = await usersService.deleteUser(req.params.id);
+    if (!users) {
+      throw new RequestError(404, "User not found");
+    }
+
+    res.json(users.map(User.toResponse));
+    return next();
+  } catch (err) {
+    return next(err);
   }
 });
 
